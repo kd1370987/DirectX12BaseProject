fb9aed26b010eacc965ace684473bc4fec13afaa
inui <kd1370987@st.kobedenshi.ac.jp>
2026-05-07 09:39:33 +0900

    レイをシステムから登録するためシステムとコンポーネントの作成

---
+++ BaseProject/Source/Application/Systems/Draw/Draw/RegisterRayWorldSystem/RegisterRayWorldSystem.cpp
@@ -0,0 +1,36 @@
+﻿#include "RegisterRayWorldSystem.h"
+
+#include "Engine/ECS/World/World.h"
+
+#include "Engine/Raytracing/RaytracingEngine/RaytracingEngine.h"
+
+#include "../../../../Components/Tag/RenderTag/RayTag.h"
+#include "../../../../Components/Resource/ModelComponent.h"
+#include "../../../../Components/Transform/WorldMatrixComponent.h"
+
+void RegisterRayWorldSystem::Init(Engine::ECS::World& a_world)
+{
+	a_world.ActiveTask<const RayTag,const ModelComponent,const WorldMatrixComponent>(
+		Engine::ECS::ESystemType::Draw,
+		[]
+		(
+			Engine::ECS::ArchetypeChunk* a_pChunk,
+			uint32_t a_count,
+			float a_dt,
+			ActiveTag* a_pTags,
+			const RayTag* a_pRayTags,
+			const ModelComponent* a_pModelArray,
+			const WorldMatrixComponent* a_pWorldMatArray
+		)
+		{
+			for (size_t _i = 0; _i < a_count; ++_i)
+			{
+				const WorldMatrixComponent& _wMatComp = a_pWorldMatArray[_i];
+				const ModelComponent& _modelComp = a_pModelArray[_i];
+
+				// レイトレワールドに記録
+				Engine::Raytracing::RayEngine::Instance().RegistModel(_wMatComp.worldMat, _modelComp.handle);
+			}
+		}
+	);
+}
---
+++ BaseProject/Source/Application/Systems/Draw/Draw/RegisterRayWorldSystem/RegisterRayWorldSystem.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "Engine/ECS/System/SystemBase/SystemBase.h"
+
+class RegisterRayWorldSystem : public Engine::ECS::SystemBase<RegisterRayWorldSystem>
+{
+public:
+
+	static constexpr Engine::ECS::ESystemType s_type = Engine::ECS::ESystemType::Draw;
+
+	void Init(Engine::ECS::World& a_world) override;
+};
