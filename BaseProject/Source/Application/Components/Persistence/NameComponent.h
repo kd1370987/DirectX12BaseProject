ef112feff3b680e2583d8711d4ed2f5f2e3181b1
inui <kd1370987@st.kobedenshi.ac.jp>
2026-05-21 15:58:39 +0900

    nameコンポーネントを記録した。

--- BaseProject/Source/Application/Components/Persistence/NameComponent.h
+++ BaseProject/Source/Application/Components/Persistence/NameComponent.h
@@ -12,7 +12,14 @@ struct NameComponent
 	static void Deserialize(void* a_ptr, const nlohmann::json& a_json)
 	{
 		NameComponent& _comp = Engine::Editor::GetValue<NameComponent>(a_ptr);
-		//_comp.name = a_json["name"].get<std::string>().c_str();
+		if (a_json.contains("name"))
+		{
+			std::string nameStr = a_json["name"].get<std::string>();
+
+			// 63文字までコピーし、最後に必ずヌル終端コード(\0)を入れる
+			size_t copied = nameStr.copy(_comp.name, sizeof(_comp.name) - 1);
+			_comp.name[copied] = '\0';
+		}
 	}
 
 	static void Edit(void* a_data)
