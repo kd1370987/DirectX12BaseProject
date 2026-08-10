452ac73095d8aa3a798f4cd58e06b8191c1f11dc
inui <kd1370987@st.kobedenshi.ac.jp>
2026-08-11 06:39:19 +0900

    カメラのパラメータをエディター上で変更できるようにした。

--- BaseProject/Source/Application/Components/Camera/CameraParamComponent.h
+++ BaseProject/Source/Application/Components/Camera/CameraParamComponent.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include "../../Components/Camera/ProjMatComponent.h"
+
 struct CameraParamComponent
 {
 	float fovY			= 60.0f;        // 垂直視野角(単位: 度)
@@ -23,9 +25,29 @@ struct Engine::ECS::ComponentTraits<CameraParamComponent>
 	static void Edit(CompEditContext& a_context)
 	{
 		CameraParamComponent& _comp = Engine::Editor::GetValue<CameraParamComponent>(a_context.pData);
-		ImGui::DragFloat("Fov", &_comp.fovY);
-		ImGui::DragFloat("Aspect", &_comp.aspectRatio);
-		ImGui::DragFloat("NearZ", &_comp.nearZ);
-		ImGui::DragFloat("FarZ", &_comp.farZ);
+
+		bool _isEdit = false;
+
+		_isEdit |= ImGui::DragFloat("Fov", &_comp.fovY);
+		_isEdit |= ImGui::DragFloat("Aspect", &_comp.aspectRatio);
+		_isEdit |= ImGui::DragFloat("NearZ", &_comp.nearZ, 0.01f, 0.1f);
+		_isEdit |= ImGui::DragFloat("FarZ", &_comp.farZ,1.0f,1.0f);
+
+		_comp.nearZ = std::max(0.1f, _comp.nearZ);
+		_comp.farZ = std::max(1.0f,_comp.farZ);
+
+		auto* _pProjMatComp = a_context.pWorld->RefData<ProjMatComponent>(a_context.entity);
+		if (!_pProjMatComp) return;
+
+		if (_isEdit)
+		{
+			DirectX::XMMATRIX _lhMat = DirectX::XMMatrixPerspectiveFovLH(
+				DirectX::XMConvertToRadians(_comp.fovY),
+				_comp.aspectRatio,
+				_comp.nearZ,
+				_comp.farZ
+			);
+			DirectX::XMStoreFloat4x4(&_pProjMatComp->projMat, _lhMat);
+		};
 	}
 };
